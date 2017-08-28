66916714542bd95001c77f36f78497809abf0754
Angela Maugey <a.maugey@gmail.com>
2017-08-28 18:46:15 +0100

    creating a map function in the video list to be able to loop through the videos returned in array

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAbU7H5w7Si3VQYQC4Sz2_bh_bwv9BFRpo';
 
@@ -25,6 +26,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
         </div>
     );
   }
