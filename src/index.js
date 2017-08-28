936d689f3d1f0d5a1796533d694dcfc6700a9f2e
Angela Maugey <a.maugey@gmail.com>
2017-08-28 17:48:32 +0100

    beginning to access data from YT api. Function created in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAbU7H5w7Si3VQYQC4Sz2_bh_bwv9BFRpo';
+
+YTSearch({key: API_KEY, term: 'beyonce'}, function(data) {
+  console.log(data);
+})
+
+
 // Create a new component. This component should produce some HTML.
 
 const App = () => {
