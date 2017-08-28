a698c4311cc691b2045403e065ee1f43022881b2
Angela Maugey <a.maugey@gmail.com>
2017-08-28 17:31:25 +0100

    updating the state when the input value is changed

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,8 +7,14 @@ class SearchBar extends Component {
 
     this.state = { term: '' };
   }
+
   render() {
-    return <input onChange={(event) => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input onChange={(event) => this.setState({ term: event.target.value })} />
+        // Value of the input: {this.state.term}
+      </div>
+    );
   }
 
 
