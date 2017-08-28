485c33beeca6b1b3279bff448f01f25504870530
Angela Maugey <a.maugey@gmail.com>
2017-08-28 14:00:46 +0100

    components created, searchbar added to index page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAbU7H5w7Si3VQYQC4Sz2_bh_bwv9BFRpo';
 // Create a new component. This component should produce some HTML.
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+    <SearchBar />
+  </div>
+  );
 }
 
 
