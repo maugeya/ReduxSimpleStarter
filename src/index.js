39ec17676163ee3202ac4647fb929c038d8061a0
Angela Maugey <a.maugey@gmail.com>
2017-08-28 18:02:57 +0100

    changing app to a class component to attach state as input data will change during course of user involvement

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAbU7H5w7Si3VQYQC4Sz2_bh_bwv9BFRpo';
 
-YTSearch({key: API_KEY, term: 'beyonce'}, function(data) {
-  console.log(data);
-})
 
 
 // Create a new component. This component should produce some HTML.
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'beyonce'}, (videos) => {
+      this.setState({ videos: videos });
+      console.log(videos);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        </div>
+    );
+  }
+
+}
 
 // Take this component's generated HTML and put it on the page (in the DOM).
 
